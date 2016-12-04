0b8dc16ea89e1d3faad9035c86dc1bcb0f30b648
Gosha Krikun <gosha@krikun.org>
2016-12-04 17:28:58 +0300

    Work on interim report

---
+++ paper/auto/thesis.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook
+ "thesis"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "inputenc"
+    "graphicx"))
+ :latex)
+
