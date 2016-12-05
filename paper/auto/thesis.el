3f633fd8bb5c91dbe1f0c1f8c9225d19d654362b
Gosha Krikun <gosha@krikun.org>
2016-12-05 23:05:10 +0300

    Break on sections, add .gitignore, images

--- paper/auto/thesis.el
+++ paper/auto/thesis.el
@@ -4,12 +4,22 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "12pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "utf8")))
+                     '(("inputenc" "utf8") ("xcolor" "dvipsnames")))
    (TeX-run-style-hooks
     "latex2e"
+    "section1"
+    "section2"
+    "section3"
+    "section4"
+    "section5"
+    "section6"
+    "section7"
     "article"
     "art12"
     "inputenc"
-    "graphicx"))
+    "graphicx"
+    "xcolor"
+    "hyperref")
+   (LaTeX-add-bibliographies))
  :latex)
 
